6098e8e814734cca1f44ca4068fefdb95275e8cf
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-01-08 15:10:55 -0600

    diseñando modal para ver la solicitud desde analisis

--- resources/views/interno/analisis/index.blade.php
+++ resources/views/interno/analisis/index.blade.php
@@ -12,6 +12,21 @@
 
 @livewire('analisis-documentos-table')
 
+<!-- CREACION DEL modal -->
+<div
+x-data="{ open:false, solicitud: {}}"
+@open-modal-solicitud.window="open = true; solicitud = $event.detail.solicitud"
+x-show="open"
+x-cloak
+class="fixed inset-0 z-50 overflow-y-auto"
+aria-cabellad="modal-title" role="dialog" aria-modal="true"
+> 
+
+<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"> 
+</div>
+
+</div>
+
 <!-- creacion del blade para ver la solicitud -->
 
 </x-interno-layout>
